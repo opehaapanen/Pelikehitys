953a60ad13fcc3f4877a06c7a94d7be4628b4291
Arto Haapanen <70745574+opehaapanen@users.noreply.github.com>
2026-05-05 13:24:23 +0300

    Create Player.cs
    Added Health component and methods for damage handling.

---
+++ Player.cs
@@ -0,0 +1,11 @@
+private Health health;
+
+void Awake()
+{
+    // TODO: hae Health-komponentti, käytä GetComponent<T> -metodia
+ }
+
+public void TakeDamage(int amount)
+{
+    // TODO: Pyydä Health luokkaa vähentämään pelaajan terveyttä
+}
